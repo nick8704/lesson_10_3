8af8e02b83a7f87e94aff41ceb810edd4b8ec90b
Alexandr Nikonets <nick8704@gmail.com>
2018-03-11 16:15:23 +0200

    second commit

--- src/com/company/TestClass.java
+++ src/com/company/TestClass.java
@@ -3,14 +3,14 @@ package com.company;
 public class TestClass {
 
     private static int a = 10;
-    private static int c = getC();
     private static int b = a;
+    private static int c = getC();
+
+    private static int result = 123;
 
     static {
         result = a + b + c;
     }
-
-    private static int result = 123;
 
     private static int getC() {
         return a + b;
